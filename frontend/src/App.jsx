b9531040df168698ed306f224244a3817ef2d596
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-01-26 18:23:50 +0530

    chore: set up initial file structure with pages and components folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import { Button } from '@material-tailwind/react'
-import React from 'react'
-
-function Home() {
+export default function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Click</Button>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default Home
