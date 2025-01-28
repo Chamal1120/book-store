a57e9f8075515f44ec46314a2247430eec5e03e0
Chamal1120 <kenway213beep@gmail.com>
2025-01-28 18:31:53 +0530

    Revert "Merge pull request #7 from AchiraNadeeshan/main"
    This reverts commit 12c246e09eddeb552e12fbdbc2b420336cdfd8f5, reversing
    changes made to d260d145b78e9ac6c4e78de1fd59cc36a1a8e4aa.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Home from './pages/home';
 import BookPage from './pages/bookPage';
 import Register from './pages/register.jsx';
