b89a5e1980d5543550e278aa23ed4c301049376e
Chamal1120 <kenway213beep@gmail.com>
2025-02-06 23:12:01 +0530

    ci/cd: add api gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -465,6 +465,67 @@ resource "aws_dynamodb_table" "purchased_books" {
 }
 
 ###############################
+# API Gateway
+###############################
+
+resource "aws_api_gateway_rest_api" "express_api" {
+  name        = "ExpressAPI"
+  description = "API Gateway for the Express app"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.express_api.id
+  parent_id   = aws_api_gateway_rest_api.express_api.root_resource_id
+  path_part   = "api"  # This will create a path like /api
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.express_api.id
+  parent_id   = aws_api_gateway_resource.api_resource.id
+  path_part   = "{proxy+}"  # This will allow for any sub-paths (e.g., /api/endpoint)
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = aws_api_gateway_rest_api.express_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "ANY"  # This allows all HTTP methods (GET, POST, etc.)
+  authorization = "NONE"  # Change to "AWS_IAM" if you want to secure it
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.express_api.id
+  resource_id             = aws_api_gateway_resource.lambda_resource.id
+  http_method             = aws_api_gateway_method.lambda_method.http_method
+  integration_http_method = "POST"  # Lambda integration method
+  type                    = "AWS_PROXY"  # Use AWS_PROXY for Lambda integration
+
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.express_app.arn}/invocations"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.express_app.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The source ARN allows the API Gateway to invoke the Lambda function
+  source_arn = "${aws_api_gateway_rest_api.express_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.express_api.id
+
+  depends_on = [aws_api_gateway_integration.lambda_integration]  # Ensure the integration is created before deployment
+}
+
+resource "aws_api_gateway_stage" "api_stage" {
+  stage_name    = "prod"  # You can change this to your desired stage name
+  rest_api_id   = aws_api_gateway_rest_api.express_api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  description = "Production stage for Express API"
+}
+
+###############################
 # Terraform Variables
 ###############################
 
