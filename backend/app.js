c6314e86e4b2465d71ed987fd83a4351f0093fde
Chamal1120 <kenway213beep@gmail.com>
2025-02-06 23:09:32 +0530

    chore: remove cors (will move the cors management to aws)

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const cors = require("cors");
 const awsServerlessExpress = require("aws-serverless-express");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, GetCommand, PutCommand, ScanCommand } = require("@aws-sdk/lib-dynamodb");
 require("dotenv").config();
 
 const app = express();
-
-// Enable CORS
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  }),
-);
 
 // JWT Secret Key
 const JWT_SECRET = process.env.JWT_SECRET || "fallback_not_secure_string";
