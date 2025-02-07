c5f6d4b21d456f469ccbc15e8ac5b6222cd56565
Chamal1120 <kenway213beep@gmail.com>
2025-02-07 16:31:40 +0530

    feat: establish functionality for cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes'); // Import the routes​
 
 // Begin Application config​
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: 'http://localhost:5173', // Specify the exact origin
+  credentials: true, // Allow credentials
+};
+
+app.use(cors(corsOptions)); // Use the CORS options
+
 app.use(bodyParser.json());
 
 // Mount routes under /api/v1/​
