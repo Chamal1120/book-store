34bfb1c419f5efbc80f959160bd026ca457d7e58
Chamal1120 <kenway213beep@gmail.com>
2025-02-09 00:41:06 +0530

    feat: add refresh button to update books on demand

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { useBooks } from '../context/BooksContext';
+import { ArrowPathIcon, ArrowRightStartOnRectangleIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/solid';
 
 const NavBar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const { refreshBooks } = useBooks();
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleLogout = async () => {
@@ -42,8 +46,8 @@ const NavBar = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           aria-label="Search books"
         />
-        <button 
-          type="submit" 
+        <button
+          type="submit"
           className="ml-2 px-4 py-2 bg-blue-600 rounded-md hover:bg-blue-700"
           aria-label="Search"
         >
@@ -71,22 +75,45 @@ const NavBar = () => {
             >
               Login
             </button>
-            
+
           </>
         ) : (
           // Show the username and Logout button if authenticated
           <>
             <div className="flex items-center space-x-4 bg-gray-200 p-0.1 rounded-lg shadow-md"></div>
-            <span className="px-4 py-2 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700">{user.username.toUpperCase()}</span>
+            <span className="px-4 py-2 bg-orange-400 text-white rounded-full cursor-pointer hover:bg-orange-600">
+                <ShoppingCartIcon className='size-5 text-white'/>
+              </span>
+            <span className="flex px-4 py-2 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700">
+              <div className="px-1">
+                <UserCircleIcon className="size-6 text-white" />
+              </div>
+              <div className="px-1">
+                {user.username}
+              </div>
+            </span>
             <button
-              className="px-4 py-2 bg-gray-600 rounded-md hover:bg-gray-700"
+              className="flex px-4 py-2 bg-gray-600 rounded-md hover:bg-gray-700"
               onClick={handleLogout}
               aria-label="Logout"
             >
-              Logout
+
+              <div className="px-1">
+                <ArrowRightStartOnRectangleIcon className="size-6 text-white" />
+              </div>
+              <div className="px-1">
+                Logout
+              </div>
             </button>
           </>
         )}
+        <button
+          className="px-4 py-2 bg-white rounded-md hover:bg-gray-300"
+          onClick={refreshBooks}
+          aria-label="Refresh Books"
+        >
+          <ArrowPathIcon className="size-5 text-gray-800" />
+        </button>
       </div>
     </nav>
   );
